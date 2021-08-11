c300c6cf7571b8fb33170f76bc7547d417cc1dd1
Nikholau <nikholasyan12@gmail.com>
2021-08-11 16:04:42 -0300

    teste

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header >
+    <h1>
+      <div className="title">Typescript CRUD</div>
+    </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
